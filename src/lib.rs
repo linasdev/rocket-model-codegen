5624880e0c35f936972698daa802640a991cf1e4
Linas Nikiperavicius <linas@linasdev.com>
2020-04-28 22:06:14 +0300

    Finish initial codegen implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,87 +1,118 @@
 use quote::{ToTokens, quote};
-use syn::{parenthesized, braced, parse_macro_input};
+use syn::{braced, parse_macro_input};
 use syn::{Visibility, Ident, Field, Fields, FieldsNamed, Token, ItemStruct, Generics, token};
 use syn::parse::{Parse, ParseStream, Result};
 use syn::punctuated::Punctuated;
 
-enum GenField {
+enum MetaField {
     Add(Field),
     Remove(Ident),
 }
 
-impl Parse for GenField {
+impl Parse for MetaField {
     fn parse(input: ParseStream) -> Result<Self> {
         Ok(
             match input.parse::<Token![-]>() {
-                Ok(_) => GenField::Remove(input.parse()?),
-                Err(_) => GenField::Add(input.call(Field::parse_named)?),
+                Ok(_) => MetaField::Remove(input.parse()?),
+                Err(_) => MetaField::Add(input.call(Field::parse_named)?),
             }
         )
     }
 }
 
-struct GenParent {
-    paren_token: token::Paren,
-    parent: Ident,
+struct MetaFields {
+    add_fields: Punctuated<Field, Token![,]>,
+    rem_fields: Vec<Ident>,
 }
 
-impl Parse for GenParent {
+impl Parse for MetaFields {
     fn parse(input: ParseStream) -> Result<Self> {
-        let content;
-        Ok(GenParent {
-            paren_token: parenthesized!(content in input),
-            parent: content.parse()?,
-        })
+        let fields: Punctuated<_, Token![,]> = Punctuated::parse_terminated(input)?;
+
+        Ok(
+            MetaFields {
+                add_fields: fields.iter().filter_map(|f| match f {
+                    MetaField::Add(a) => Some(a.clone()),
+                    _ => None,
+                }).collect(),
+                rem_fields: fields.iter().filter_map(|f| match f {
+                    MetaField::Remove(r) => Some(r.clone()),
+                    _ => None,
+                }).collect(),
+            }
+        )
     }
 }
 
-struct GenStruct {
+struct MetaStruct {
     visibility: Visibility,
     struct_token: Token![struct],
     name: Ident,
-    parent: Option<GenParent>,
     generics: Generics,
     brace_token: token::Brace,
-    fields: Punctuated<GenField, Token![,]>,
+    fields: MetaFields,
 }
 
-impl Parse for GenStruct {
+impl Parse for MetaStruct {
     fn parse(input: ParseStream) -> Result<Self> {
         let content;
-        Ok(GenStruct {
+        Ok(MetaStruct {
             visibility: input.parse()?,
             struct_token: input.parse()?,
             name: input.parse()?,
-            parent: input.parse().ok(),
             generics: input.parse()?,
             brace_token: braced!(content in input),
-            fields: content.parse_terminated(GenField::parse)?,
+            fields: content.parse()?,
         })
     }
 }
 
-struct GenStructs(Punctuated<GenStruct, Token![,]>);
+struct MetaStructs(Punctuated<MetaStruct, Token![,]>);
 
-impl Parse for GenStructs {
+impl Parse for MetaStructs {
     fn parse(input: ParseStream) -> Result<Self> {
-        Ok(GenStructs(Punctuated::parse_terminated(input)?))
+        Ok(MetaStructs(Punctuated::parse_terminated(input)?))
     }
 }
 
-impl ToTokens for GenStructs {
+impl ToTokens for MetaStructs {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        for gen_struct in &self.0 {
-            let named = Punctuated::new();
+        for (i, meta_struct) in self.0.iter().enumerate() {
+            let mut named = Punctuated::new();
+
+            'outer: for field in &meta_struct.fields.add_fields {
+                for rem_field in &meta_struct.fields.rem_fields {
+                    if field.ident.as_ref().unwrap().eq(rem_field) {
+                        continue 'outer;
+                    }
+                }
+
+                named.push(field.clone());
+            }
+
+            if i != 0 {
+                let parent_fields = &self.0.first().unwrap().fields;
+
+                'parent_outer: for field in &parent_fields.add_fields {
+                    for rem_field in &meta_struct.fields.rem_fields {
+                        if field.ident.as_ref().unwrap().eq(rem_field) {
+                            continue 'parent_outer;
+                        }
+                    }
+    
+                    named.push(field.clone());
+                }
+            }
 
             ItemStruct {
                 attrs: vec![],
-                vis: gen_struct.visibility.clone(),
-                struct_token: gen_struct.struct_token,
-                ident: gen_struct.name.clone(),
-                generics: gen_struct.generics.clone(),
+                vis: meta_struct.visibility.clone(),
+                struct_token: meta_struct.struct_token,
+                ident: meta_struct.name.clone(),
+                generics: meta_struct.generics.clone(),
                 fields: Fields::Named(FieldsNamed {
-                    brace_token: gen_struct.brace_token,
-                    named
+                    brace_token: meta_struct.brace_token,
+                    named,
                 }),
                 semi_token: None,
             }.to_tokens(tokens);
@@ -91,13 +122,13 @@ impl ToTokens for GenStructs {
 
 #[proc_macro]
 pub fn gen_structs(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let gen_structs = parse_macro_input!(input as GenStructs);
+    let meta_structs = parse_macro_input!(input as MetaStructs);
 
     let token_stream = quote! {
-        #gen_structs
+        #meta_structs
     };
 
-    println!("{}", token_stream.to_string());
+    println!("{}", token_stream);
 
     token_stream.into()
 }
